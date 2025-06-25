49a39eb9f62c7b4a9cc85d445754ce0af2af20dc
Mukul Mahendra Dhobale <mukuldhobale92@gmail.com>
2025-06-25 19:26:30 +0530

    added sidebar,code editor,chatting application

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -5,6 +5,7 @@ import Signup from './Pages/Signup';
 import Dashboard from './Pages/Dashboard';
 import LandingPage from './Pages/Landingpage';
 import ForgotPassword from './Pages/ForgotPassword';
+import CodeCollabPage from "./Pages/CodeCollabPage";
 
 const App: React.FC = () => {
   return (
@@ -15,6 +16,7 @@ const App: React.FC = () => {
         <Route path="/signup" element={<Signup />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/CodeCollabPage" element={<CodeCollabPage />} />
       </Routes>
     </Router>
   );
